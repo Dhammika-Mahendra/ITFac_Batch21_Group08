b257a2f98c6d6edc5d5b40c84124933eb0ef3811
dhammikamahendra <wmdhammikamahendra@gmail.com>
2026-01-29 21:08:22 +0530

    all cat returned by admin test

--- cypress/e2e/API/category/api.category.js
+++ cypress/e2e/API/category/api.category.js
@@ -1,6 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { apiLoginAsAdmin, apiLoginAsUser } from "../../preconditions/login";
-import { getAllCategories } from "../../../support/api/category";
+import { createCategory, getAllCategories, deleteCategory } from "../../../support/api/category";
 
 Given("I have logged in as an admin user", () => {
 	return apiLoginAsAdmin();
@@ -9,6 +9,8 @@ Given("I have logged in as an admin user", () => {
 Given("I have logged in as a non-admin user", () => {
     return apiLoginAsUser();
 });
+
+// @Cat_Admin_API_01 -----------------------------------------------
 
 When("I call the categories get API point", () => {
 	return getAllCategories();
@@ -21,5 +23,32 @@ Then("I should receive a 200 status code", () => {
 Then("the response should contain a list of categories", () => {
 	return cy.get("@categoriesResponse").then((response) => {
 		expect(response.body, "categories payload").to.be.an("array");
+
+		//verify main categories exist (parentName = null)
+		const mainCategories = response.body.filter(category => category.parentName === "-");
+		expect(mainCategories.length, "main categories count").to.be.greaterThan(0);
+
+		//verify subcategories exist (parentName != null)
+		const subCategories = response.body.filter(category => category.parentName !== null);
+		expect(subCategories.length, "subcategories count").to.be.greaterThan(0);
+	});
+});
+
+// @Cat_Admin_API_03 -----------------------------------------------
+
+When("I send a request to create a new category", () => {
+	let data = {"id":null,"name":"Jannet","parent":{"id":13,"name":null,"parent":null}};
+	return createCategory(data, "createCategoryResponse");
+});
+
+Then("the category should be created successfully", () => {
+	return cy.get("@createCategoryResponse").then((response) => {
+		expect(response.status, "create category status").to.eq(201);
+
+		// Clean up: delete the created category
+		const createdCategoryId = response.body.id;
+		if (createdCategoryId) {
+			return deleteCategory(createdCategoryId);
+		}
 	});
 });
